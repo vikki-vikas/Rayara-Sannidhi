ba61613bf998da4b422f7a3f566111e47974ad10
Vikas TU <vikastuvikki@gmail.com>
2026-05-03 14:45:35 +0530

    deploy added

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: '/Rayara-Sannidhi',
+  assetPrefix: '/Rayara-Sannidhi/',
   /* config options here */
 };
 
